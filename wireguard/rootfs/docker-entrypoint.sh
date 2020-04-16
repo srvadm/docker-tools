aede140e55b8e301e944661963164a9fa976bdec
tinonaumann <tino@naumix.de>
2020-04-16 19:59:05 +0200

    u

--- wireguard/rootfs/docker-entrypoint.sh
+++ wireguard/rootfs/docker-entrypoint.sh
@@ -1,5 +1,27 @@
 
+if ! [ -f '/etc/wireguard/wg0.conf.src' ]; then
+cat << EOF > /etc/wireguard/wg0.conf.src
+[Interface]
+Address = 10.10.1.1/24
+PrivateKey = %privkey%
+ListenPort = 51820
+PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE;iptables -A FORWARD -o %i -j ACCEPT
+PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE;iptables -D FORWARD -o %i -j ACCEPT
+
+[Peer]
+PublicKey = %pubkey%
+AllowedIPs = 10.10.1.10
+fi
+EOF
+fi
+
+#if [ -f '/etc/wireguard/wg0.conf' ] && [ "$@" == "start" ]; then
+#  wg-quick up wg0
+#  while true
+#  do
+#    continue
+#  done
+#fi
 
 echo "$@"
 
-"$@"
